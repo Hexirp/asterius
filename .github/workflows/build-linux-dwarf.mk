a11ed2beaa99caf157e8f05b80cf7e3cb8e43137
Cheng Shao <cheng.shao@tweag.io>
2020-08-28 18:15:34 +0200

    WIP

--- .github/workflows/build-linux-dwarf.mk
+++ .github/workflows/build-linux-dwarf.mk
@@ -1,8 +1,8 @@
 SRC_HC_OPTS               = -O -H1024m
 GhcStage1HcOpts           = -O
 GhcStage2HcOpts           = -O2 -g3 -fprof-auto
-GhcRtsHcOpts              = -O2 -g3 -fprof-auto
-GhcLibHcOpts              = -O2 -g3 -fprof-auto
+GhcRtsHcOpts              = -O2 -g3
+GhcLibHcOpts              = -O2 -g3
 BUILD_PROF_LIBS           = YES
 DYNAMIC_TOO               = NO
 DYNAMIC_GHC_PROGRAMS      = NO
